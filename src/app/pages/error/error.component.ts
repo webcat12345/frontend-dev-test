f1b1aadf2a72964cfd466513f3b8985904e7bacf
webcat12345 <webcat91@gmail.com>
2018-07-23 22:21:53 +0200

    update project setting * upgrade angular version to latest

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
-import { Subscription } from 'rxjs/Subscription';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-error',
